500db23c1ff8f1155ea71eb3c2c5ee7a70abf5d6
eyern <eyernfuture@gmail.com>
2019-07-12 21:57:08 +0300

    include client id on the profile service

---
+++ src/app/services/profile.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+ private username: string;
+ private clientid "" '#48479784';
+ private clientsecret "" 
+
+
+  constructor(private http:Http) {
+  
+   }
+}
