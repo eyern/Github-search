e41cf0778035d0b05de5a20bf7fdf4a175638fcb
eyern <eyernfuture@gmail.com>
2019-07-15 06:51:03 +0300

    add a map to the ts

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -7,12 +7,17 @@ import 'rxjs/add/operator/map';
 })
 export class ProfileService {
 
- private username: string;
- private clientid "" '#48479784';
- private clientsecret "" 
+ private username:string;
+ private clientid =	'f3571dBe55e1695400825';
+ private clientsecret =	'a6c2b7d8f7c691e771b19008873e1322028e97c4'; 
 
 
   constructor(private http:Http) {
-  
-   }
+  	console.log("service is now ready!");
+  	this.username = 'kirandash';
+  	}
+  	getProfileInfo(){
+  		return this.http.get("https:api.github.com/users/"	+ this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+  	.map(res => res.json());	
+  	}
 }
